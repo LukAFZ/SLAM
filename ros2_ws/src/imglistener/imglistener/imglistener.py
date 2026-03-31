eb245e638180198b311a69f21931dfa0962050fe
TimBau0 <tib8820@thi.de>
2026-03-31 16:21:00 +0200

    3D-Koordinaten

--- ros2_ws/src/imglistener/imglistener/imglistener.py
+++ ros2_ws/src/imglistener/imglistener/imglistener.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+import math
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
@@ -17,7 +18,11 @@ class ImageSubscriber(Node):
         # Initiate ORB detector
         self.orb = cv2.ORB_create()
 
-
+        # Image center coordinates
+        self.cu = 318.525
+        self.cv = 241.181
+        # Focal length 
+        self.f = 526.61
 
 
     def listener_callback_rgb(self,msg):
@@ -25,8 +30,7 @@ class ImageSubscriber(Node):
         
         # find the keypoints with ORB
         kp = self.orb.detect(frame,None)
-        # compute the descriptors with ORB
-        kp, des = self.orb.compute(frame, kp)
+        
         
         
         kp_clean = []
@@ -42,7 +46,25 @@ class ImageSubscriber(Node):
                     text = f"{depth:.2f} m"
                     cv2.putText(frame, text, (x + 5, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
                     kp_clean.append(point)
-        
+
+        # compute the descriptors with ORB
+        kp_clean, des_clean = self.orb.compute(frame, kp_clean)
+
+        for point in kp_clean:
+            depth = self.depth_frame[int(point.pt[1]), int(point.pt[0])]
+            u = self.cu - point.pt[0]
+            v = point.pt[1]-self.cv
+
+            phi1 = math.atan2(u, self.f)
+            phi2 = math.atan2(v, self.f)
+            x = depth * math.tan(phi1)
+            y = depth * math.tan(phi2)
+            z = depth
+            
+            control_u = self.f*(x/z)
+            control_v = self.f*(y/z)
+    
+            print(f"Real coordinates: ({(u):.2f}, {(v):.2f}), Control commands: ({control_u:.2f}, {control_v:.2f}), Angles: ({math.degrees(phi1):.2f} deg, {math.degrees(phi2):.2f} deg)")
         # draw keypoints in green
         img2 = cv2.drawKeypoints(frame, kp_clean, None, color=(0,255,0), flags=0)
         cv2.imshow("Feature + Depth",img2)
@@ -52,23 +74,23 @@ class ImageSubscriber(Node):
             # create BFMatcher object
             bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
             # Match descriptors.
-            matches = bf.match(self.des_queue, des)
+            matches = bf.match(self.des_queue, des_clean)
             # Sort them in the order of their distance.
             matches = sorted(matches, key = lambda x:x.distance)
             # Draw first 10 matches.
-            #img3 = cv2.drawMatches(frame, kp, frame, kp, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+            #img3 = cv2.drawMatches(frame, kp_clean, frame, kp_clean, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
             #cv2.imshow("Matches",img3)
             #cv2.waitKey(1)
 
         
         # store current descriptors for next frame matching
-        self.des_queue = des
+        self.des_queue = des_clean
         
 
     def listener_callback_depth(self, msg):
         self.depth_frame=self.bridge.imgmsg_to_cv2(msg,'passthrough')
-        cv2.imshow("Depth",self.depth_frame)
-        cv2.waitKey(1)
+        #cv2.imshow("Depth",self.depth_frame)
+        #cv2.waitKey(1)
     
 
 def main():
