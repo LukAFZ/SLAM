5bb2c7ea5921523e04805cf462de16bd4b28d1c6
LukAFZ <l.ziebarth2@gmail.com>
2026-04-28 12:11:12 +0200

    Update zu Tim

--- src/imglistener/imglistener/imglistener.py
+++ src/imglistener/imglistener/imglistener.py
@@ -2,17 +2,30 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import PointCloud2
+from geometry_msgs.msg import TransformStamped
+import std_msgs.msg
+import sensor_msgs_py.point_cloud2 as pcl2
 from cv_bridge import CvBridge
 import cv2
 import math
+import numpy as np
+import time
+
 class ImageSubscriber(Node):
     def __init__(self):
         super().__init__('image_subscriber')
         self.bridge = CvBridge()
+        # Subscribe to RGB image topic
         self.subscription = self.create_subscription(Image,
             '/serf01/nav_rgbd_1/rgb/image_raw', self.listener_callback_rgb, 10)
+        # Subscribe to depth image topic
         self.subscription = self.create_subscription(Image,
             '/serf01/nav_rgbd_1/depth/image_raw', self.listener_callback_depth, 10)
+        # Publisher for 3D pointcloud
+        self.pcl_publisher = self.create_publisher(PointCloud2, '/serf01/nav_rgbd_1/pointcloud', 10)
+        
+        
         self.des_queue = None
         self.depth_frame = None
         # Initiate ORB detector
@@ -23,6 +36,101 @@ class ImageSubscriber(Node):
         self.cv = 241.181
         # Focal length 
         self.f = 526.61
+        self.frame_counter = 10
+        self.min_matches = 50
+
+        # Store 3D points
+        self.current_points_3d = []
+        self.current_descriptors = []
+        self.point_history = []
+
+        self.keyframes = []
+        self.frame_index = 0
+        self.to_proceed_frames = 1
+        self.queue_index = 0
+        self.sum_t = 0
+        self.sum_deg = 0
+
+    def get_kapsch_2d(self, P, Q):    
+
+        # Calculate the centroids of P and Q
+        P_middle = np.mean(P, axis=0) #p_quer
+        Q_middle = np.mean(Q, axis=0) #q_quer
+
+        # Center the points by subtracting the centroids  
+        P_centered = P - P_middle #p_strich
+        Q_centered = Q - Q_middle #p_strich
+
+
+        # Calculate the rotation angle (theta) using the Kabsch algorithm
+        theta = math.atan2(sum(Q_centered[:,0]*P_centered[:,1] - Q_centered[:,1]*P_centered[:,0]), sum(Q_centered[:,0]*P_centered[:,0] + Q_centered[:,1]*P_centered[:,1]))
+        #print(f"Rotation angle (theta): {math.degrees(theta):.2f} degrees")
+
+        # Calculate the rotation matrix using the rotation angle
+        Rotation_matrix = np.array([[math.cos(theta), -math.sin(theta)],
+                                    [math.sin(theta), math.cos(theta)]])
+        # Calculate the translation vector using the centroids and the rotation matrix
+        Translation = P_middle - Rotation_matrix @ Q_middle
+        #print(f"Translation vector: {Translation}")
+        return Rotation_matrix, Translation, theta
+
+
+
+    def ransac_refinement(self, P, Q):
+        
+        max_iterations= 200
+        threshold= 50
+        best_rotation = None
+        best_translation = None
+        best_theta = 0
+        best_inlier_count = 0
+        
+       
+        
+        if(len(P) < 5):
+            # Not enough points for RANSAC, return the transformation from all points
+            return best_rotation, best_translation, best_theta
+
+        for _ in range(max_iterations):
+            # Randomly select a subset of points
+            P_second = []
+            Q_second = []
+
+            indices = np.random.choice(len(P), size=3, replace=False)
+            P_subset = P[indices]
+            Q_subset = Q[indices]
+
+            # Estimate the transformation using the selected subset
+            R_estimated, t_estimated, theta_estimated = self.get_kapsch_2d(P_subset, Q_subset)
+
+ 
+            # Transform Q and calculate per-point errors
+            Q_transformed = (R_estimated @ Q.T).T + t_estimated
+            errors = np.linalg.norm(P - Q_transformed, axis=1)
+            
+            inlier_count = np.sum(errors < threshold)
+
+
+
+            if inlier_count > best_inlier_count:
+                for e, p, q in zip(errors, P, Q):
+                    if e < threshold:
+                        P_second.append(p)
+                        Q_second.append(q)
+
+                best_rotation, best_translation, best_theta = self.get_kapsch_2d(np.array(P_second), np.array(Q_second))
+
+                best_inlier_count = inlier_count
+                
+
+        #print("Inliner percentage: {:.2f}%".format(best_inlier_count / len(P) * 100))
+
+
+        return best_rotation, best_translation, best_theta
+
+# odom berechnen
+# odom publisher und in RVIZ2 anschauen
+
 
 
     def listener_callback_rgb(self,msg):
@@ -31,6 +139,8 @@ class ImageSubscriber(Node):
         # find the keypoints with ORB
         kp = self.orb.detect(frame,None)
         
+        matrix_zero_3d = []
+        matrix_second_3d = []
         
         
         kp_clean = []
@@ -41,50 +151,126 @@ class ImageSubscriber(Node):
                 # get x,y coordinates of keypoint
                 x, y = int(point.pt[0]), int(point.pt[1])
                 # get depth value at keypoint location and convert to meters
-                depth = self.depth_frame[y, x]/1000.0
-                if(depth > 0.4 and depth < 4): # filter out invalid depth values
-                    text = f"{depth:.2f} m"
+                depth = self.depth_frame[y, x]
+                if(depth > 400 and depth < 4500): # filter out invalid depth values
+                    text = f"{depth:.1f} mm"
                     cv2.putText(frame, text, (x + 5, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
                     kp_clean.append(point)
-
+        else:
+            return # skip processing if depth frame is not available
         # compute the descriptors with ORB
         kp_clean, des_clean = self.orb.compute(frame, kp_clean)
+        # for i in range(len(kp_clean)):
+        #   point = kp_clean[i]
+        #   des = des_clean[i]
 
-        for point in kp_clean:
+        # for point, des in zip(kp_clean, des_clean):
+        #   ...
+        if(kp_clean is None or des_clean is None):
+            return # skip processing if no valid keypoints/descriptors are found
+
+        for point, des in zip(kp_clean, des_clean):
             depth = self.depth_frame[int(point.pt[1]), int(point.pt[0])]
             u = self.cu - point.pt[0]
             v = point.pt[1]-self.cv
 
-            phi1 = math.atan2(u, self.f)
-            phi2 = math.atan2(v, self.f)
-            x = depth * math.tan(phi1)
-            y = depth * math.tan(phi2)
+
+            # 3D coordinates calculation
+            x = (point.pt[0] - self.cu) * depth / self.f
+            y = (point.pt[1] - self.cv) * depth / self.f
             z = depth
-            
-            control_u = self.f*(x/z)
-            control_v = self.f*(y/z)
-    
-            print(f"Real coordinates: ({(u):.2f}, {(v):.2f}), Control commands: ({control_u:.2f}, {control_v:.2f}), Angles: ({math.degrees(phi1):.2f} deg, {math.degrees(phi2):.2f} deg)")
+
+            #control_u = self.f*(x/z)
+            #control_v = self.f*(y/z)
+            #print(f"Real coordinates: ({(u):.2f}, {(v):.2f}), Control commands: ({control_u:.2f}, {control_v:.2f})")
+            self.current_points_3d.append((x, y, z))
+            self.current_descriptors.append(des)
+        points_np = np.array(self.current_points_3d)
+        des_np = np.array(self.current_descriptors)
+
+        # store keyframe data
+        self.keyframes.append({
+            'points_3d': points_np,
+            'des': des_np
+        })
+
         # draw keypoints in green
         img2 = cv2.drawKeypoints(frame, kp_clean, None, color=(0,255,0), flags=0)
         cv2.imshow("Feature + Depth",img2)
         cv2.waitKey(1)
+        # publish 3D points as PointCloud2 message
+        header = std_msgs.msg.Header()
+        header.stamp = self.get_clock().now().to_msg()
+        header.frame_id = 'kinect_depth'
+        points_in_meters = [(p[0]/1000, p[1]/1000, p[2]/1000) for p in self.current_points_3d] # convert from mm to meters
+        pointcloud_msg = pcl2.create_cloud_xyz32(header, points_in_meters) # only publish x,y,z coordinates, ignore descriptors
+        self.pcl_publisher.publish(pointcloud_msg)
+        # TF tree missing
+        # ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 base_link feature_points
 
-        if(self.des_queue is not None):
-            # create BFMatcher object
-            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-            # Match descriptors.
-            matches = bf.match(self.des_queue, des_clean)
-            # Sort them in the order of their distance.
-            matches = sorted(matches, key = lambda x:x.distance)
-            # Draw first 10 matches.
-            #img3 = cv2.drawMatches(frame, kp_clean, frame, kp_clean, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-            #cv2.imshow("Matches",img3)
-            #cv2.waitKey(1)
+        # empty the 3D points list for the next frame
+        self.current_points_3d = []
+        self.current_descriptors = []
+        
+
+        # if(self.des_queue is not None):
+        #     # create BFMatcher object
+        #     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        #     # Match descriptors.
+        #     matches = bf.match(self.des_queue, des_clean)
+        #     # Sort them in the order of their distance.
+        #     matches = sorted(matches, key = lambda x:x.distance)
+        #     # Draw first 10 matches.
+        #     #img3 = cv2.drawMatches(frame, kp_clean, frame, kp_clean, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+        #     #cv2.imshow("Matches",img3)
+        #     #cv2.waitKey(1)
 
         
-        # store current descriptors for next frame matching
-        self.des_queue = des_clean
+
+        if(self.frame_counter == 0):
+            if(self.des_queue is not None):
+                bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+                matches = bf.match(self.des_queue, des_clean)
+                if(len(matches) > self.min_matches):
+                    for match in matches:
+                        idx1 = match.queryIdx
+                        idx2 = match.trainIdx
+                        matrix_zero_3d.append(self.keyframes[self.queue_index]['points_3d'][idx1])
+                        matrix_second_3d.append(points_np[idx2])
+                    
+                    matrix_zero = np.delete(matrix_zero_3d, 1, axis=1) # remove y coordinate
+                    matrix_second = np.delete(matrix_second_3d, 1, axis=1) # remove y coordinate
+                    R, t, theta = self.ransac_refinement(matrix_zero, matrix_second)
+                    print(f"Estimated rotation (theta): {math.degrees(theta):.2f} degrees")
+                    print(f"Estimated translation: {t}")
+
+                    self.sum_t += t
+                    self.sum_deg += math.degrees(theta)
+                    print(f"Sum of translations: {self.sum_t}, Sum of rotations: {self.sum_deg:.2f} degrees")
+
+
+                    tf = TransformStamped()
+                    tf.header.stamp = self.get_clock().now().to_msg()
+                    tf.header.frame_id = 'odom'
+                    tf.child_frame_id = 'kinect_depth'
+                    tf.transform.translation.x = t[0]
+                    tf.transform.translation.y = 0
+                    tf.transform.translation.z = t[1]
+                    tf.transform.rotation = euler_to_quaternion(0, theta, 0)
+
+                    # kinect_depth [x, 0, z]
+                    # rotation = euler_to_quaternion(0, theta, 0)
+                else:
+                    print(f"Not enough matches found for RANSAC {len(matches)}")
+            self.des_queue = des_clean
+            self.queue_index = self.frame_index
+            self.frame_counter = self.to_proceed_frames    
+
+
+        self.frame_index += 1
+        self.frame_counter -= 1
+        
+        
         
 
     def listener_callback_depth(self, msg):
